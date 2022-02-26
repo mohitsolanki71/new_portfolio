c0a414e767d4ee2a055ae7e1408a88ed35cf3bf8
mohitsolanki71 <91531587+mohitsolanki71@users.noreply.github.com>
2022-02-26 22:32:19 +0530

    updated project

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,132 +1,147 @@
 //I Would Highly Recommend You To Customize The Theme According To Your Taste.
 export const globalStyles = {
-
-    "--grad": "linear-gradient(315deg,#48a9fe,#00cdac)", // Use gradients only
-    // "--grad": "linear-gradient(315deg, #48a9fe 0%, #734ae8 74%)", // Use gradients only
-    // "--grad": "linear-gradient(315deg, #06bcfb 0%, #4884ee 74%)",
-    "--txtcolor": "#272341",
-    "--p_color": "#00cdac",
-    "--s_color": "#fff"
-}
+  "--grad": "linear-gradient(315deg,#48a9fe,#00cdac)", // Use gradients only
+  // "--grad": "linear-gradient(315deg, #48a9fe 0%, #734ae8 74%)", // Use gradients only
+  // "--grad": "linear-gradient(315deg, #06bcfb 0%, #4884ee 74%)",
+  "--txtcolor": "#272341",
+  "--p_color": "#00cdac",
+  "--s_color": "#fff",
+};
 
 // Navigation Bar Name
-export const navbar_name = "Mohit"
+export const navbar_name = "Mohit";
 
 // Main Page
 export const main = {
-    name: "Mohit Solanki",
-    doamin: "Full Stack Web Developer",
-    resume: "https://drive.google.com/file/d/11iXKQny42Jhhrq2y71d81kqKaG-BRg_W/view?usp=sharing"
-}
+  name: "Mohit Solanki",
+  doamin: "Full Stack Web Developer",
+  resume:
+    "https://drive.google.com/file/d/11iXKQny42Jhhrq2y71d81kqKaG-BRg_W/view?usp=sharing",
+};
 
 // AboutMe
 export const aboutme = {
-    imgLink: "https://github.com/mohitsolanki71/portfolio_images/blob/main/images/img.jpg?raw=true",
-    p1: "Hey,My name is Mohit Solanki. A passionate individual who always thrive to work on end to end products",
-    p2: "I'm a Tech Enthusiast who enjoys building awesome website that solves real world problems.",
-    p3: "A self-motivated team worker looking to work in an organization where he can contribute his skills and grow in his career."
-}
+  imgLink:
+    "https://github.com/mohitsolanki71/portfolio_images/blob/main/images/img.jpg?raw=true",
+  p1: "Hey,My name is Mohit Solanki. A passionate individual who always thrive to work on end to end products",
+  p2: "I'm a Tech Enthusiast who enjoys building awesome website that solves real world problems.",
+  p3: "A self-motivated team worker looking to work in an organization where he can contribute his skills and grow in his career.",
+};
 
 // Skills
 export const whatido = {
-    title: "Full Stack Development",
+  title: "Full Stack Development",
 
-    details: ["Building responsive website using React",
-        "Creating backend application in Node, Express",
-        "Building Simple RESTful CRUD APIs",
-        "Knowledge of Object-Oriented programming languages like JavaScript"],
-    logos: [{
-        name: "HTML5",
-        iconifyClassName: "vscode-icons:file-type-html", // PS : Use iconify class name
-        id: "1"
+  details: [
+    "Building responsive website using React",
+    "Creating backend application in Node, Express",
+    "Building Simple RESTful CRUD APIs",
+    "Knowledge of Object-Oriented programming languages like JavaScript",
+  ],
+  logos: [
+    {
+      name: "HTML5",
+      iconifyClassName: "vscode-icons:file-type-html", // PS : Use iconify class name
+      id: "1",
     },
     {
-        name: "CSS",
-        iconifyClassName: "vscode-icons:file-type-css",
-        id: "2"
+      name: "CSS",
+      iconifyClassName: "vscode-icons:file-type-css",
+      id: "2",
     },
     {
-        name: "Tailwind",
-        iconifyClassName: "logos:tailwindcss-icon",
-        id: "3"
+      name: "Tailwind",
+      iconifyClassName: "logos:tailwindcss-icon",
+      id: "3",
     },
     {
-        name: "JavaScript",
-        iconifyClassName: "logos:javascript",
-        id: "4"
+      name: "JavaScript",
+      iconifyClassName: "logos:javascript",
+      id: "4",
     },
     {
-        name: "ReactJS",
-        iconifyClassName: "vscode-icons:file-type-reactjs",
-        id: "5"
+      name: "ReactJS",
+      iconifyClassName: "vscode-icons:file-type-reactjs",
+      id: "5",
     },
     {
-        name: "Redux",
-        iconifyClassName: "logos:redux",
-        id: "6"
+      name: "Redux",
+      iconifyClassName: "logos:redux",
+      id: "6",
     },
     {
-        name: "NodeJS",
-        iconifyClassName: "logos:nodejs",
-        id: "7"
+      name: "NodeJS",
+      iconifyClassName: "logos:nodejs",
+      id: "7",
     },
     {
-        name: "MongoDB",
-        iconifyClassName: "logos:mongodb",
-        id: "8"
+      name: "MongoDB",
+      iconifyClassName: "logos:mongodb",
+      id: "8",
     },
     {
-        name: "NPM",
-        iconifyClassName: "logos:npm-icon",
-        id: "9"
+      name: "NPM",
+      iconifyClassName: "logos:npm-icon",
+      id: "9",
     },
     {
-        name: "VSCode",
-        iconifyClassName: "vscode-icons:file-type-vscode",
-        id: "10"
+      name: "VSCode",
+      iconifyClassName: "vscode-icons:file-type-vscode",
+      id: "10",
     },
     {
-        name: "PostMan",
-        iconifyClassName: "logos:postman-icon",
-        id: "11"
+      name: "PostMan",
+      iconifyClassName: "logos:postman-icon",
+      id: "11",
     },
     {
-        name: "Git",
-        iconifyClassName: "bi:git",
-        id: "12"
+      name: "Git",
+      iconifyClassName: "bi:git",
+      id: "12",
     },
     {
-        name: "Vercel",
-        iconifyClassName: "logos:vercel",
-        id: "13"
-    }
-    ]
-}
+      name: "Vercel",
+      iconifyClassName: "logos:vercel",
+      id: "13",
+    },
+  ],
+};
 
 // Project Details
-export const details = [{
+export const details = [
+  {
     id: 1,
     title: "Clone of Reliance-Retail",
     desc: "An informative website that provides information about various brands in the Reliance group. The features we included in this clone are : Brand information pages and links to respective brand pages | Product purchase feature for Hamleys brand | Sort and filter feature for Hamley’s products",
     img: "https://github.com/mohitsolanki71/portfolio_images/blob/main/images/reliance(1).jpg?raw=true",
     github: "https://github.com/Shivam2101s/Reliance-retails",
     link: "https://relianceretailclone.netlify.app/",
-    deploy:"https://relianceretailclone.netlify.app/index.html"
-    
-},
-{
+    deploy: "https://relianceretailclone.netlify.app/index.html",
+  },
+  {
     id: 2,
     title: "Clone of SmallCase Website",
-    desc: "A web application that has a basket of stocks that a user can invest in as a complete packet or customize it as per their requirements. The features we included in this website clone are : Allows to invest in a smallcase | User authentication using APIs | User authentication using APIs | News updates about the stock market",
+    desc: "A web application that has a basket of stocks that a user can invest in as a complete packet or customize it as per their requirements. The features we included in this website clone are : Allows to invest in a smallcase | User authentication using APIs | User authorization using APIs | News updates about the stock market",
     img: "https://github.com/RajP62/Smallcase/blob/main/public/images/dashboard.png?raw=true",
     link: "https://smallcaseproject.herokuapp.com/home",
     github: "https://github.com/RajP62/Smallcase",
-    deploy:"https://smallcaseproject.herokuapp.com/home"
-}
-]
+    deploy: "https://smallcaseproject.herokuapp.com/home",
+  },
+  {
+    id: 3,
+    title: "Clone of FABALLEY Website",
+    desc: "A web application that is based on women fashion accessories and allows users to choose products from a wide range of varriety . The features we included in this website clone are : Allows user to Register and Login | User authentication using APIs | Get details of specific product | Can Purchase Product | Can Make Payment",
+    img: "https://user-images.githubusercontent.com/53556482/153560974-2ee2f508-b57a-4af8-87be-17139579613d.png",
+    link: "https://faballey-clone.vercel.app/",
+    github: "https://github.com/Sachi4496/faballey-clone",
+    deploy: "https://faballey-clone.vercel.app/",
+  },
+];
 
 // Social Media Links
-export const links = ["https://www.linkedin.com/in/mohitsolanki71/",
-    "https://github.com/mohitsolanki71",
-    "https://www.instagram.com/mohitsolanki71/",
-    "mailto:mohitsolanki071@gmail.com"]
+export const links = [
+  "https://www.linkedin.com/in/mohitsolanki71/",
+  "https://github.com/mohitsolanki71",
+  "https://www.instagram.com/mohitsolanki71/",
+  "mailto:mohitsolanki071@gmail.com",
+];
